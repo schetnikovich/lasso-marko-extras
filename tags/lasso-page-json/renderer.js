83b221edbc68bb3c79ae24708b274cb3b89c24ac
schetnikovich <tschetnikovich@gmail.com>
2016-11-07 21:00:21 +0300

    <lasso-page-json /> tag

---
+++ tags/lasso-page-json/renderer.js
@@ -0,0 +1,48 @@
+/**
+ * Defines <app-page /> tag, wrapper around <lasso-page /> tag.
+ *
+ * The only difference is that it reads configuration from "page.json" file,
+ * located in the same folder where template is located. This allows to
+ * build static assets with the same configuration options.
+ *
+ * See "src/build.js" for build process.
+ *
+ * Example:
+ *
+ *   {
+ *     "name": "about",
+ *     "packagePath": "./browser.json",
+ *     "cacheKey": "some-unique-key"
+ *   }
+ */
+
+var lassoPageTag = require('lasso/taglib/page-tag');
+var path = require('path');
+var fs = require('fs');
+
+exports.tag = {
+  'no-input': true,
+  'import-var': {
+    'dirname': '__dirname',
+    'filename': '__filename'
+  }
+};
+
+var cache = {};
+
+exports.renderer = function(input, out) {
+  // Path to custom "page.json" config
+  var configPath = path.join(input.dirname, 'page.json');
+
+  // Cache config content (actually, we cache only call to "fs.existsSync())
+  if (cache[configPath] === undefined) {
+    var exists = fs.existsSync(configPath);
+    cache[configPath] = exists ? require(configPath) : {};
+  }
+
+  var config = cache[configPath];
+  config.dirname = input.dirname;
+  config.filename = input.filename;
+  lassoPageTag(config, out);
+};
+
