c9ef79d8bad2cd6005906e0d311a1f6964eb8d35
schetnikovich <tschetnikovich@gmail.com>
2016-11-08 01:27:54 +0300

    Assets builder changes

--- test/site/build.js
+++ test/site/build.js
@@ -5,25 +5,12 @@ var extras = require('../../lib');
 
 require('raptor-logging').configure({
   loggers: {
-    'lasso-marko-extras': 'DEBUG'
+    'lasso-marko-extras': 'INFO'
   }
 });
 
-// Specify any files that needs to be copied to assets folder (build/static)
-var files = [
-  'layouts/base/favicon.ico'
-];
-
-// Specify pages
-var pages = [
-  'pages/home'
-];
-
-extras.process(__dirname, files, pages, function(err) {
-  if (err) {
-    console.log('Failed to process assets!');
-    throw err;
-  }
-
-  console.log('Assets processed!');
+extras.build({
+  baseDir: __dirname,
+  files: ['layouts/base/favicon.ico'],
+  pages: ['pages/home']
 });
